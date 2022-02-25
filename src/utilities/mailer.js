fa501c7d5816c9806b38af488591c725bbd52612
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-02-25 02:13:09 +0100

    Fixed email verification link

--- src/utilities/mailer.js
+++ src/utilities/mailer.js
@@ -216,7 +216,7 @@ const htmlTemplates = ({ user, emailContent }) => {
                 <!-- start copy -->
                 <tr>
                   <td align="left" bgcolor="#ffffff" style="padding: 24px; font-family: 'Source Sans Pro', Helvetica, Arial, sans-serif; font-size: 16px; line-height: 24px; border-bottom: 3px solid #d4dadf">
-                    <p style="margin: 0;">Cheers,<br> Paste</p>
+                    <p style="margin: 0;">Cheers,<br> ${user.name}</p>
                   </td>
                 </tr>
                 <!-- end copy -->
