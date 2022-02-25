3e94d552ff553fb88beeb35b7c8e092d28ee9763
Olayiwola Sobowale <layitheinfotechguru@gmail.com>
2022-02-25 02:51:43 +0100

    Remove conversion to timestamp

--- src/data/schema.js
+++ src/data/schema.js
@@ -2,17 +2,31 @@ const { gql } = require("apollo-server-express");
 
 const typeDefs = gql`
   scalar Date
+  """
+  A Fidia user
+  """
   type User {
+    "Fidia's user unique ID"
     id: ID
+    "To determine if the user is still active based on activities on Fidia platform"
     isActive: Boolean
+    "To determine if a soft delete is performed on the user due to data science"
     isDeleted: Boolean
+    "The date of creation of Fidia user account"
     createdOn: Date
+    "The date of last update performed on the user"
     updatedOn: Date
+    "Fidia's user name"
     name: String
+    "Fidia's user email"
     email: String
+    "Fidia's user mobile number"
     mobileNumber: String
+    "To determine if the user has verified their account via email"
     emailVerified: Boolean
+    "The Fidia's user password which is hashed due to security reasons"
     password: String
+    "The Fidia's user country of location"
     country: String
   }
 
